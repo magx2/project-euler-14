d15299825abd750705aeb1281ba59bd1f76c8407
Martin Grześlowski <martin.grzeslowski@gmail.com>
2013-12-03 22:59:20 +0100

    Added 1 case to test

--- project-euler-14/src/test/java/pl/grzeslowski/project_euler_14/ProjectEuler14PartitionTest.java
+++ project-euler-14/src/test/java/pl/grzeslowski/project_euler_14/ProjectEuler14PartitionTest.java
@@ -21,7 +21,7 @@ public class ProjectEuler14PartitionTest {
 
     @Parameters
     public static Collection<Object[]> data() {
-        final Object[][] data = new Object[][] { { 1, 4L } };
+        final Object[][] data = new Object[][] { { 1, 4L }, {3, 8L} };
         return Arrays.asList(data);
     }
 
